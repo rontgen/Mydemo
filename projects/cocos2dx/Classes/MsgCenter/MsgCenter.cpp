105356cfea74090166282a879efe2e871f6e6c48
rontgen <minstrelgen@163.com>
2016-01-21 23:10:00 +0800

    [init project]
    1. add msg center but with some errors to deal with

---
+++ projects/cocos2dx/Classes/MsgCenter/MsgCenter.cpp
@@ -0,0 +1,43 @@
+#include "MsgCenter.h"
+
+
+USING_NS_CC;
+
+MsgCenter::MsgCenter()
+    : m_mapListener()
+    , m_list()
+{
+
+}
+
+MsgCenter::~MsgCenter()
+{
+    m_mapListener.clear();
+    m_list.clear();
+}
+
+void MsgCenter::addObserver(const std::string& msg, Callback func)
+{
+    if (m_mapListener.find(msg)!= m_mapListener.end())
+    {
+        return;
+    }
+    //m_mapListener.insert(std::make_pair(msg, func));
+}
+
+void MsgCenter::sendMsg(const std::string& msg, const cocos2d::Ref* pSender)
+{
+    if (m_mapListener.find(msg) != m_mapListener.end())
+    {
+        auto func = m_mapListener[msg];
+        func(pSender);
+    }
+}
+
+void MsgCenter::update(float delta)
+{
+    //1.mutex thread
+    //2. check if m_queue is empty
+    //3. if not empty, pop msg and sendMsg
+}
+
---
+++ projects/cocos2dx/Classes/MsgCenter/MsgCenter.h
@@ -0,0 +1,33 @@
+/**
+  * author: rontgen
+  * Func Description: 
+  */
+#pragma once
+
+#include "cocos2d.h"
+#include "CCComponent.h"
+#include <functional>
+#include <list>
+#include <map>
+typedef const std::function<void(const cocos2d::Ref*)>& Callback;
+class MsgCenter : public cocos2d::Component
+{
+public:
+    static MsgCenter* instance()
+    {
+        static MsgCenter ins;
+        return &ins;
+    }
+    void update(float delta) override;
+    void addObserver(const std::string& msg, Callback func);
+    void sendMsg(const std::string& msg, const cocos2d::Ref* pSender);
+private:
+    MsgCenter();
+    ~MsgCenter();
+    MsgCenter(const MsgCenter*);
+    MsgCenter& operator = (const MsgCenter*);
+private:
+    std::map<const std::string&, Callback> m_mapListener;
+    std::list<const std::string&> m_list;
+};
+
